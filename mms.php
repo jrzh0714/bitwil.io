b67e42aef3b8c9c63259375d2f669462c0140562
Jerry Zheng <jiaruizheng0714@gmail.com>
2016-05-11 22:12:55 +0000

    bitwil.io

---
+++ mms.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<?php
+$FromNumber = "+15162094484"; // number that will send
+$ToNumber = $_GET['phoneNumber']; // number that will recieve 
+$Msg = $_GET['link'];  
+    require '/Users/scripted/Downloads/twilio-php-master/Services/Twilio.php';
+$AccountSid = "AC37d521a58b09365069bd823f792581e4";
+$AuthToken = "e9d1b03ff13730b187fa421b32aad44e";
+$client = new Services_Twilio($AccountSid, $AuthToken);
+$pic = array($_GET['imgLink']);
+$bSuccess = $client->account->messages->sendMessage(
+    
+    $FromNumber, 	
+    $ToNumber,           
+    $Msg,	
+    $pic		
+);
+echo "Sent message to $ToNumber ";
+
+echo $Msg;
+?>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+    var login = "o_3g1eg10l7a";
+var api_key = "R_c3742ed47b4b43ada3c6c53207c4f490";
+var long_url = document.getElementById("url");
+function bit_url(url) 
+{ 
+    var url=url;
+    var username="o_3g1eg10l7a"; // bit.ly username
+    var key="R_c3742ed47b4b43ada3c6c53207c4f490";
+$.ajax({
+    url:"http://api.bit.ly/v3/shorten",
+    data:{longUrl:url,apiKey:key,login:username},
+    dataType:"jsonp",
+    success:function(v)
+{
+    var bit_url=v.data.url;
+    var phoneNumber = $("#phoneNumber").val();
+    var imgLink = $("#imgLink").val();
+    $("#result").html('<a href="'+"http://localhost:8888/mms.php?link="+bit_url+"&phoneNumber="+phoneNumber+"&imgLink="+imgLink+'" target="_blank">'+bit_url+'</a>');
+    }
+    });
+    }
+
+
+$("#short").click(function()
+{
+var url=$("#url").val();
+var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+var urltest=urlRegex.test(url);
+if(urltest)
+{
+bit_url(url);
+}
+else
+{
+alert("Bad URL");
+}
+});
+
+});
