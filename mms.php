23c93e2090218cf2d9a82ce0ade4716b5c0552d3
Jerry Zheng <jiaruizheng0714@gmail.com>
2016-05-18 21:44:33 +0000

    fixed

--- mms.php
+++ mms.php
@@ -5,8 +5,8 @@ $FromNumber = "+15162094484"; // number that will send
 $ToNumber = $_GET['phoneNumber']; // number that will recieve 
 $Msg = $_GET['link'];  
     require '/Services/Twilio.php';
-$AccountSid = "SKac60182826f843a8fea1d99eaa52b981";
-$AuthToken = "1Xzjr72rdnkZXX4zneDJxqKaBSONH2Yg";
+$AccountSid = "AC37d521a58b09365069bd823f792581e4";
+$AuthToken = "e9d1b03ff13730b187fa421b32aad44e";
 $client = new Services_Twilio($AccountSid, $AuthToken);
 $pic = array($_GET['imgLink']);
 $bSuccess = $client->account->messages->sendMessage(
